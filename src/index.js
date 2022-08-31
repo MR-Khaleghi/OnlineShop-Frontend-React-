01248052c28a7aa0f42e87b934530d7862344eec
MR-Khaleghi <rasool_khe@yahoo.com>
2022-08-31 23:21:53 +0430

    new directory the first add -A

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+
+// ReactDOM.render(
+// //   <React.StrictMode>
+    
+    
+    
+// //   </React.StrictMode>
+// , document.querySelector('#root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+       <App />
+
+  </React.StrictMode>
+);
